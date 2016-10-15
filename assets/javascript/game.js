e9de6a8b36a744eaf3a81635246074614a86d604
Alberto Alvarez <albertoealvarezm1992@gmail.com>
2016-10-15 19:21:37 -0400

    html and user input javascript added

---
+++ assets/javascript/game.js
@@ -0,0 +1,17 @@
+//Javascript game code 
+
+
+document.onkeyup = function (event) {
+
+	var words = ["anakin","yoda","death star","jedi","force"];
+
+	var userguess = String.fromCharCode(event.keyCode).toLowerCase();
+    console.log(userguess);
+
+    
+
+
+
+
+}
+
